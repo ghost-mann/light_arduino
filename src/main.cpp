be60746b673453b6f77881bebe7a2e6c07a54a8a
ghost-mann <iamaob8@protonmail.com>
2026-01-13 11:56:05 +0300

    feat: defined color function and wrote it to rgb led pins in rgb.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <Arduino.h>
+#include "rgb.h"
 
 void setup() {
-
+  init_rgb();
 }
 
 void loop() {
+  loop_rgb();
 
 }
 
--- src/rgb.cpp
+++ src/rgb.cpp
@@ -1,14 +1,30 @@
 #include <Arduino.h>
 #include "rgb.h"
 
- int redPin = 9;
- int bluePin = 10;
- int greenPin = 11; 
+ int redPin = 11;
+ int bluePin = 9;
+ int greenPin = 10;
+
+ void color (unsigned char red, unsigned char green, unsigned char blue) {
+    analogWrite(redPin,red);
+    analogWrite(bluePin,blue);
+    analogWrite(greenPin,green);
+}
+ 
 
 void init_rgb() {
-
+    pinMode(redPin,OUTPUT);
+    pinMode(bluePin,OUTPUT);
+    pinMode(greenPin,OUTPUT);
 }
 
 void loop_rgb() {
+    color(255,0,0);
+    delay(5000);
+    color(0,255,0);
+    delay(5000);
+    color(0,0,255);
+    delay(5000);
 
 }
+
