98959a81d21fb8a5a6142bdedaabc7a4ac20b88c
ghost-mann <iamaob8@protonmail.com>
2026-01-13 12:16:28 +0300

    feat: added dataByte variable to register user input via serialMonitor manipulating modes of rgb bulb in rgb.cpp

--- src/rgb.cpp
+++ src/rgb.cpp
@@ -4,6 +4,7 @@
  int redPin = 11;
  int bluePin = 9;
  int greenPin = 10;
+ int dataByte = 0;
 
  void color (unsigned char red, unsigned char green, unsigned char blue) {
     analogWrite(redPin,red);
@@ -16,15 +17,26 @@ void init_rgb() {
     pinMode(redPin,OUTPUT);
     pinMode(bluePin,OUTPUT);
     pinMode(greenPin,OUTPUT);
+    Serial.begin(9600);
 }
 
 void loop_rgb() {
-    color(255,0,0);
-    delay(5000);
-    color(0,255,0);
-    delay(5000);
-    color(0,0,255);
-    delay(5000);
+    if (Serial.available() > 0 ) {
+        dataByte = Serial.read();
+
+        color(255,0,0);
+        delay(1000);
+        color(0,255,0);
+        delay(1000);
+        color(0,0,255);
+        delay(1000);
+    } else {
+        color(237,109,0);
+        delay(1000);        
+    }
+    Serial.print("I received:");
+    Serial.println(dataByte);
+
 
 }
 
